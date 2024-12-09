19e1dbd1b4d37006d314f910b26bc09ab800bace
dariantonova <daricio1256@gmail.com>
2024-12-09 13:28:13 +0300

    Fix paths

--- src/settings.ts
+++ src/settings.ts
@@ -5,7 +5,7 @@ config();
 export const SETTINGS = {
     PORT: process.env.PORT || 3003,
     PATH: {
-        BLOGS: 'api/blogs',
-        POSTS: 'api/posts',
+        BLOGS: '/api/blogs',
+        POSTS: '/api/posts',
     }
 }
