ce351b6bc86e246c6dbdfd8dfe89e31dc67774db
dariantonova <daricio1256@gmail.com>
2024-12-09 17:38:25 +0300

    Add tests for delete blog endpoint

--- __tests__/blogs.e2e.test.ts
+++ __tests__/blogs.e2e.test.ts
@@ -50,4 +50,47 @@ describe('tests for /blogs', () => {
             .get(SETTINGS.PATH.BLOGS + '/2')
             .expect(HTTP_STATUSES.OK_200, blogs[1]);
     });
+
+    it('should forbid deleting blogs for non-admin users', async () => {
+        await req
+            .delete(SETTINGS.PATH.BLOGS + '/1')
+            .expect(HTTP_STATUSES.UNAUTHORIZED_401);
+    });
+
+    it('should return 404 when deleting non-existing blog', async () => {
+        await req
+            .delete(SETTINGS.PATH.BLOGS + '/-100')
+            .set('Authorization', 'Basic YWRtaW46cXdlcnR5')
+            .expect(HTTP_STATUSES.NOT_FOUND_404);
+    });
+
+    it('should delete the first blog', async () => {
+        await req
+            .delete(SETTINGS.PATH.BLOGS + '/1')
+            .set('Authorization', 'Basic YWRtaW46cXdlcnR5')
+            .expect(HTTP_STATUSES.NO_CONTENT_204);
+
+        await req
+            .get(SETTINGS.PATH.BLOGS + '/1')
+            .expect(HTTP_STATUSES.NOT_FOUND_404);
+
+        await req
+            .get(SETTINGS.PATH.BLOGS + '/2')
+            .expect(HTTP_STATUSES.OK_200, blogs[0]);
+    });
+
+    it('should delete the second blog', async () => {
+        await req
+            .delete(SETTINGS.PATH.BLOGS + '/2')
+            .set('Authorization', 'Basic YWRtaW46cXdlcnR5')
+            .expect(HTTP_STATUSES.NO_CONTENT_204);
+
+        await req
+            .get(SETTINGS.PATH.BLOGS + '/2')
+            .expect(HTTP_STATUSES.NOT_FOUND_404);
+
+        await req
+            .get(SETTINGS.PATH.BLOGS)
+            .expect(HTTP_STATUSES.OK_200, []);
+    });
 });
