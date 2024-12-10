df27ea61d53c0580666e482504b76669fc7de012
dariantonova <daricio1256@gmail.com>
2024-12-10 10:54:46 +0300

    Make tests more isolated

--- __tests__/blogs.e2e.test.ts
+++ __tests__/blogs.e2e.test.ts
@@ -2,7 +2,7 @@ import {req} from "./test-helpers";
 import {SETTINGS} from "../src/settings";
 import {encodeToBase64, HTTP_STATUSES} from "../src/utils";
 import {BlogDBType} from "../src/types";
-import {setDB} from "../src/db/db";
+import {db, setDB} from "../src/db/db";
 import {getValidAuthValue} from "../src/middlewares/authorization-middleware";
 import * as dataset from './dataset';
 
@@ -12,99 +12,140 @@ describe('tests for /blogs', () => {
             .delete(SETTINGS.PATH.TESTING + '/all-data');
     });
 
-    it('should return empty array', async () => {
-        await req
-            .get(SETTINGS.PATH.BLOGS)
-            .expect(HTTP_STATUSES.OK_200, []);
+    it('database should be cleared', async () => {
+        expect(db.blogs.length).toBe(0);
+        expect(db.posts.length).toBe(0);
     });
 
-    let blogs: BlogDBType[];
-    it('should return array with all blogs', async () => {
-        blogs = dataset.blogs;
-        setDB({ blogs });
+    describe('get blogs', () => {
+        let blogs: BlogDBType[];
 
-        await req
-            .get(SETTINGS.PATH.BLOGS)
-            .expect(HTTP_STATUSES.OK_200, blogs);
+        afterAll(async () => {
+            await req
+                .delete(SETTINGS.PATH.TESTING + '/all-data');
+        });
+
+        it('should return empty array', async () => {
+            await req
+                .get(SETTINGS.PATH.BLOGS)
+                .expect(HTTP_STATUSES.OK_200, []);
+        });
+
+        it('should return array with all blogs', async () => {
+            blogs = dataset.blogs;
+            setDB({ blogs });
+
+            await req
+                .get(SETTINGS.PATH.BLOGS)
+                .expect(HTTP_STATUSES.OK_200, blogs);
+        });
     });
 
-    it('should return 404 for non-existing blog', async () => {
-        await req
-            .get(SETTINGS.PATH.BLOGS + '/-100')
-            .expect(HTTP_STATUSES.NOT_FOUND_404);
+    describe('get blog', () => {
+        let blogs: BlogDBType[];
+
+        beforeAll(() => {
+            blogs = dataset.blogs;
+            setDB({ blogs });
+        });
+
+        afterAll(async () => {
+            await req
+                .delete(SETTINGS.PATH.TESTING + '/all-data');
+        });
+
+        it('should return 404 for non-existing blog', async () => {
+            await req
+                .get(SETTINGS.PATH.BLOGS + '/-100')
+                .expect(HTTP_STATUSES.NOT_FOUND_404);
+        });
+
+        it('should return the second blog', async () => {
+            await req
+                .get(SETTINGS.PATH.BLOGS + '/2')
+                .expect(HTTP_STATUSES.OK_200, blogs[1]);
+        });
     });
 
-    it('should return the second blog', async () => {
-        await req
-            .get(SETTINGS.PATH.BLOGS + '/2')
-            .expect(HTTP_STATUSES.OK_200, blogs[1]);
-    });
+    describe('delete blog', () => {
+        let blogs: BlogDBType[];
 
-    it('should forbid deleting blogs for non-admin users', async () => {
-        await req
-            .delete(SETTINGS.PATH.BLOGS + '/1')
-            .expect(HTTP_STATUSES.UNAUTHORIZED_401);
+        beforeAll(() => {
+            blogs = dataset.blogs;
+            setDB({ blogs });
+        });
 
-        await req
-            .delete(SETTINGS.PATH.BLOGS + '/1')
-            .set('Authorization', 'Basic somethingWierd')
-            .expect(HTTP_STATUSES.UNAUTHORIZED_401);
+        afterAll(async () => {
+            await req
+                .delete(SETTINGS.PATH.TESTING + '/all-data');
+        });
 
-        await req
-            .delete(SETTINGS.PATH.BLOGS + '/1')
-            .set('Authorization', 'Basic ')
-            .expect(HTTP_STATUSES.UNAUTHORIZED_401);
+        it('should forbid deleting blogs for non-admin users', async () => {
+            await req
+                .delete(SETTINGS.PATH.BLOGS + '/1')
+                .expect(HTTP_STATUSES.UNAUTHORIZED_401);
 
-        const credentials = SETTINGS.CREDENTIALS.LOGIN + ':' + SETTINGS.CREDENTIALS.PASSWORD;
-        await req
-            .delete(SETTINGS.PATH.BLOGS + '/1')
-            .set('Authorization', `Bearer ${encodeToBase64(credentials)}`)
-            .expect(HTTP_STATUSES.UNAUTHORIZED_401);
+            await req
+                .delete(SETTINGS.PATH.BLOGS + '/1')
+                .set('Authorization', 'Basic somethingWierd')
+                .expect(HTTP_STATUSES.UNAUTHORIZED_401);
 
-        await req
-            .delete(SETTINGS.PATH.BLOGS + '/1')
-            .set('Authorization', encodeToBase64(credentials))
-            .expect(HTTP_STATUSES.UNAUTHORIZED_401);
+            await req
+                .delete(SETTINGS.PATH.BLOGS + '/1')
+                .set('Authorization', 'Basic ')
+                .expect(HTTP_STATUSES.UNAUTHORIZED_401);
 
-        await req
-            .get(SETTINGS.PATH.BLOGS + '/1')
-            .expect(HTTP_STATUSES.OK_200, blogs[0]);
-    });
+            const credentials = SETTINGS.CREDENTIALS.LOGIN + ':' + SETTINGS.CREDENTIALS.PASSWORD;
+            await req
+                .delete(SETTINGS.PATH.BLOGS + '/1')
+                .set('Authorization', `Bearer ${encodeToBase64(credentials)}`)
+                .expect(HTTP_STATUSES.UNAUTHORIZED_401);
 
-    it('should return 404 when deleting non-existing blog', async () => {
-        await req
-            .delete(SETTINGS.PATH.BLOGS + '/-100')
-            .set('Authorization', getValidAuthValue())
-            .expect(HTTP_STATUSES.NOT_FOUND_404);
-    });
+            await req
+                .delete(SETTINGS.PATH.BLOGS + '/1')
+                .set('Authorization', encodeToBase64(credentials))
+                .expect(HTTP_STATUSES.UNAUTHORIZED_401);
 
-    it('should delete the first blog', async () => {
-        await req
-            .delete(SETTINGS.PATH.BLOGS + '/1')
-            .set('Authorization', getValidAuthValue())
-            .expect(HTTP_STATUSES.NO_CONTENT_204);
+            await req
+                .get(SETTINGS.PATH.BLOGS + '/1')
+                .expect(HTTP_STATUSES.OK_200, blogs[0]);
+        });
 
-        await req
-            .get(SETTINGS.PATH.BLOGS + '/1')
-            .expect(HTTP_STATUSES.NOT_FOUND_404);
+        it('should return 404 when deleting non-existing blog', async () => {
+            await req
+                .delete(SETTINGS.PATH.BLOGS + '/-100')
+                .set('Authorization', getValidAuthValue())
+                .expect(HTTP_STATUSES.NOT_FOUND_404);
+        });
 
-        await req
-            .get(SETTINGS.PATH.BLOGS + '/2')
-            .expect(HTTP_STATUSES.OK_200, blogs[1]);
-    });
+        it('should delete the first blog', async () => {
+            await req
+                .delete(SETTINGS.PATH.BLOGS + '/1')
+                .set('Authorization', getValidAuthValue())
+                .expect(HTTP_STATUSES.NO_CONTENT_204);
 
-    it('should delete the second blog', async () => {
-        await req
-            .delete(SETTINGS.PATH.BLOGS + '/2')
-            .set('Authorization', getValidAuthValue())
-            .expect(HTTP_STATUSES.NO_CONTENT_204);
+            await req
+                .get(SETTINGS.PATH.BLOGS + '/1')
+                .expect(HTTP_STATUSES.NOT_FOUND_404);
 
-        await req
-            .get(SETTINGS.PATH.BLOGS + '/2')
-            .expect(HTTP_STATUSES.NOT_FOUND_404);
+            await req
+                .get(SETTINGS.PATH.BLOGS + '/2')
+                .expect(HTTP_STATUSES.OK_200, blogs[1]);
+        });
 
-        await req
-            .get(SETTINGS.PATH.BLOGS)
-            .expect(HTTP_STATUSES.OK_200, []);
+        it('should delete the second blog', async () => {
+            await req
+                .delete(SETTINGS.PATH.BLOGS + '/2')
+                .set('Authorization', getValidAuthValue())
+                .expect(HTTP_STATUSES.NO_CONTENT_204);
+
+            await req
+                .get(SETTINGS.PATH.BLOGS + '/2')
+                .expect(HTTP_STATUSES.NOT_FOUND_404);
+
+            await req
+                .get(SETTINGS.PATH.BLOGS)
+                .expect(HTTP_STATUSES.OK_200, []);
+        });
     });
 });
