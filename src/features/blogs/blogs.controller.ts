54a838bfb1a477aca14bd56e28d7fdace4878fe3
dariantonova <daricio1256@gmail.com>
2024-12-10 10:09:03 +0300

    Move router handlers to controller

---
+++ src/features/blogs/blogs.controller.ts
@@ -0,0 +1,42 @@
+import {BlogDBType, RequestWithParams} from "../../types";
+import {BlogViewModel} from "./models/BlogViewModel";
+import {Request, Response} from "express";
+import {blogsRepository} from "./blogs.repository";
+import {URIParamsBlogIdModel} from "./models/URIParamsBlogIdModel";
+import {HTTP_STATUSES} from "../../utils";
+
+const mapBlogToViewModel = (dbBlog: BlogDBType): BlogViewModel => {
+    return {
+        id: dbBlog.id,
+        name: dbBlog.name,
+        description: dbBlog.description,
+        websiteUrl: dbBlog.websiteUrl,
+    };
+};
+
+export const blogsController = {
+    getBlogs: (req: Request, res: Response<BlogViewModel[]>) => {
+        const foundBlogs = blogsRepository.findBlogs();
+
+        res.json(foundBlogs.map(mapBlogToViewModel));
+    },
+    getBlog: (req: RequestWithParams<URIParamsBlogIdModel>,
+              res: Response<BlogViewModel>) => {
+        const foundBlog = blogsRepository.findBlogById(req.params.id);
+        if (!foundBlog) {
+            res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+            return;
+        }
+
+        res.json(mapBlogToViewModel(foundBlog));
+    },
+    deleteBlog: (req: RequestWithParams<URIParamsBlogIdModel>, res: Response) => {
+        const isDeleted = blogsRepository.deleteBlog(req.params.id);
+        if (!isDeleted) {
+            res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+            return;
+        }
+
+        res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+    },
+};
--- src/features/blogs/blogs.router.ts
+++ src/features/blogs/blogs.router.ts
@@ -1,49 +1,15 @@
-import {Router, Request, Response} from 'express';
-import {blogsRepository} from "./blogs.repository";
-import {BlogDBType, RequestWithParams} from "../../types";
-import {BlogViewModel} from "./models/BlogViewModel";
-import {URIParamsBlogIdModel} from "./models/URIParamsBlogIdModel";
-import {HTTP_STATUSES} from "../../utils";
+import {Router} from 'express';
 import {authorizationMiddleware} from "../../middlewares/authorization-middleware";
-
-const mapBlogToViewModel = (dbBlog: BlogDBType): BlogViewModel => {
-    return {
-        id: dbBlog.id,
-        name: dbBlog.name,
-        description: dbBlog.description,
-        websiteUrl: dbBlog.websiteUrl,
-    };
-};
+import {blogsController} from "./blogs.controller";
 
 const router = Router();
 
 router.get('/',
-    (req: Request, res: Response<BlogViewModel[]>) => {
-    const foundBlogs = blogsRepository.findBlogs();
-
-    res.json(foundBlogs.map(mapBlogToViewModel));
-});
+    blogsController.getBlogs);
 router.get('/:id',
-    (req: RequestWithParams<URIParamsBlogIdModel>,
-     res: Response<BlogViewModel>) => {
-    const foundBlog = blogsRepository.findBlogById(req.params.id);
-    if (!foundBlog) {
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
-        return;
-    }
-
-    res.json(mapBlogToViewModel(foundBlog));
-});
+    blogsController.getBlog);
 router.delete('/:id',
     authorizationMiddleware,
-    (req: RequestWithParams<URIParamsBlogIdModel>, res: Response) => {
-    const isDeleted = blogsRepository.deleteBlog(req.params.id);
-    if (!isDeleted) {
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
-        return;
-    }
-
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-});
+    blogsController.deleteBlog);
 
 export { router as blogsRouter };
